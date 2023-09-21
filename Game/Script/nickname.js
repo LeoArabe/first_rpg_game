d540e3b9bb8c82cc4d8bcee6b89ac72c3b9c89ea
LeoArabe <103582132+LeoArabe@users.noreply.github.com>
2023-09-21 02:27:53 +0000

    Pending changes exported from your codespace

---
+++ Game/Script/nickname.js
@@ -0,0 +1,17 @@
+export function handleNickname() {
+    let playerNickName = '';
+    const nickName = document.querySelector("#nickNameTool");
+    
+    nickName.innerText = 'Click aqui e escolha o seu Nickname!';
+
+    nickName.addEventListener('click', () => {
+        playerNickName = prompt("Clique e diga seu Nickname?");   
+        nickName.innerText = playerNickName;
+    });
+}
+
+
+
+
+
+
